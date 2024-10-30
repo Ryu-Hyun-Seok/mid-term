910032293d2ec2046efd5f8013b087668d219549
Ryu-Hyun-Seok <0802peter@gmail.com>
2024-10-31 02:37:48 +0900

    중간고사 계산기

---
+++ MidTermCalc.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * 중간고사 계산기 만들기
+ */
+public class MidTermCalc extends JFrame {
+    MidTermCalc() {
+        setTitle("계산기");
+
+        setLayout(new BorderLayout());
+        showCenter(); showSouth();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 500);
+        setVisible(true);
+    }
+    void showCenter() {
+
+    }
+    void showSouth() {
+
+    }
+    public static void main(String[] args) {}
+}
